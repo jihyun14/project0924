3bf8a330d11c1bf09f31816294bbb339a6b21cd2
jihyun14 <ji_hyun99@naver.com>
2025-09-24 15:17:40 +0900

    Project0924

---
+++ Project0924/src/main/java/com/example/demo/Project0924Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project0924Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project0924Application.class, args);
+	}
+
+}
